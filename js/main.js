e1f4d5133206ec99889f4cc3aa9ea0d1bfc246ae
Tyler <tylergusmyers@gmail.com>
2022-08-09 16:23:39 -0700

    //

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const themeTab = '.theme-tab';
 const switcherBtn = '.switcher-btn';
 const dark = 'dark';
 const light = 'light';
-const open = 'open'; 
+const open = 'open';
 const active = 'active';
 
 const modalOpen = '[data-open]';
@@ -12,78 +12,147 @@ const modalClose = '[data-close]';
 const isVisible = 'is-visible';
 
 const dataFilter = '[data-filter]';
+const portfolioData = '[data-item]';
 
 const root = document.documentElement;
 
-// Theme //
+/* Theme */
+
 const toggleTheme = document.querySelector(themeTab);
-const switcher= document.querySelectorAll(switcherBtn);
+const switcher = document.querySelectorAll(switcherBtn);
 const currentTheme = localStorage.getItem(theme);
 
-// Portfolio //
-const filterLink = docment.querySelectorAll(dataFilter);
+/* Portfolio */
 
-// Modal // 
+const filterLink = document.querySelectorAll(dataFilter);
+const portfolioItems = document.querySelectorAll(portfolioData);
+const searchBox = document.querySelector('#search');
+
+
+/* Modal */
+
 const openModal = document.querySelectorAll(modalOpen);
 const closeModal = document.querySelectorAll(modalClose);
 
 const setActive = (elm, selector) => {
-   if (document.querySelector(`${selector}.${active}`) !== null) {
-      document.querySelector(`${selector}.${active}`).classList.remove(active);
-   }
-   elm.classList.add(active);
-}
+    if (document.querySelector(`${selector}.${active}`) !== null) {
+        document.querySelector(`${selector}.${active}`).classList.remove(active);
+    } 
+    elm.classList.add(active);
+};
 
 const setTheme = (val) => {
-   if (val === dark) {
-      root.setAttribute(dataTheme, dark);
-      localStorage.setItem(theme, dark);
-   } else {
-      root.setAttribute(dataTheme, light);
-      localStorage.setItem(theme, light);
-   }
-}
+    if (val === dark) {
+        root.setAttribute(dataTheme, dark); 
+        localStorage.setItem(theme, dark);
+        
+    } else {
+        root.setAttribute(dataTheme, light);
+        localStorage.setItem(theme,light);
+    }
+};
 
 if (currentTheme) {
-   root.setAttribute(dataTheme, currentTheme);
-   switcher.forEach((btn) => {
-      btn.classList.remove(active);
-   });
+    root.setAttribute(dataTheme, currentTheme); 
+    switcher.forEach((btn) => {
+        btn.classList.remove(active);
+    });
+    if (currentTheme === dark ) {
+        switcher[1].classList.add(active);
 
-   if (currentTheme === dark) {
-      switcher[1].classList.add(active);
-   } else {
-      switcher[0].classList.add(active);
-   }
-}
+    } else {
+        switcher[0].classList.add(active);
 
-toggleTheme.addEventListener('click', function() {
-   const tab = this.parentElement.parentElement;
-   if (!tab.className.includes(open)) {
-      tab.classList.add(open);
-   } else {
-      tab.classList.remove(open);
-   }
-})
+    }
+};
+
+
+toggleTheme.addEventListener('click', function () {
+    const tab = this.parentElement.parentElement;
+    if (!tab.className.includes(open)) {
+        tab.classList.add(open);
+    } else {
+        tab.classList.remove(open);
+    }
+});
 
 for (const elm of switcher) {
-   elm.addEventListener('click', function() {
-      const toggle = this.dataset.toggle;
-      setActive(elm, switcherBtn);
-      setTheme(toggle);
-   })
-}
+    elm.addEventListener('click', function() {
+        const toggle = this.dataset.toggle;
+        setActive(elm, switcherBtn);
+        setTheme(toggle);
+    })
+};
 
-// Full Site Modal "open buttons" //
+searchBox.addEventListener('keyup', (e) => {
+    const searchInput = e.target.value.toLowerCase().trim();
+    
+
+    portfolioItems.forEach((card) => {
+        if (card.dataset.item.includes(searchInput)) {
+            card.style.display = 'block';
+        } else {
+            card.style.display = 'none';
+        }
+    });
+});
+
+for (const link of filterLink) {
+    link.addEventListener('click', function() {
+        setActive(link, '.filter-link'); 
+        const filter = this.dataset.filter;
+        portfolioItems.forEach((card) => {
+            if (filter === 'all') {
+                card.style.display = 'block';
+            } else if (card.dataset.item  === filter) {
+                card.style.display = 'block';
+            } else {
+                card.style.display= 'none';
+            }
+        })
+    })
+};
+
+
+
+// Full Site Modal "open buttons"
 for (const elm of openModal) {
-   elm.addEventListener('click', function() {
-      const modalId = this.dataset.open;
-      document.getElementById(modalId).classList.add(isVisible);
-   })
+    elm.addEventListener('click', function() {
+        const modalId = this.dataset.open;
+        document.getElementById(modalId).classList.add(isVisible);
+})
 }
 
 for (const elm of closeModal) {
-   elm.addEventListener('click', function() {
-      this.parentElement.parentElement.remove(isVisible);
-   })
-} 
+    elm.addEventListener('click', function() {
+    this.parentElement.parentElement.parentElement.classList.remove(isVisible);
+})
+}
+
+// Modal 
+document.addEventListener('click', (e) => {
+    if (e.target ===  document.querySelector('.modal.is-visible')) {
+        document.querySelector('.modal.is-visible').classList.remove(isVisible);
+    }
+});
+
+document.addEventListener('keyup', (e) => {
+    
+    if (e.key === 'Escape' ) {
+     document.querySelector('.modal.is-visible').classList.remove(isVisible);
+    }
+});
+
+const elmsDisplayed = getComputedStyle(root).getPropertyValue('--marquee-elms-displayed');
+const marqueeContent = document.querySelector('ul.marquee-content');
+
+root.style.setProperty('--marquee-elms', marqueeContent.children.length);
+
+for (let i = 0 ; i < elmsDisplayed; i += 1 ) {
+    marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
+    
+// get elements displayed 
+
+// nodelist.length
+// assign --marquee-elms nodelist.length
